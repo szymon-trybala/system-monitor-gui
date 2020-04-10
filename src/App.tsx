f8fd40278de019c752032322c0be766cdbe9f146
Norvos <adrian42603@gmail.com>
2020-04-10 21:25:24 +0200

    Burger menu fix, add some style

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import * as React from 'react';
-import { Component } from 'react';
-import { push as Menu } from 'react-burger-menu';
-import { NavLink, Route, Switch } from 'react-router-dom';
-import { Home } from './components/Home';
-import './styles/burger-menu.css';
+import * as React from "react";
+import { Component } from "react";
+import { push as Menu } from "react-burger-menu";
+import { NavLink, Route, Switch } from "react-router-dom";
+import { Home } from "./components/Home";
+import "./styles/burger-menu.css";
 
 export default class App extends Component {
   public render() {
     return (
-      <div id='outer-container'>
-        <Menu pageWrapId={'page-wrap'} outerContainerId={'outer-container'}>
-          <ul>
-            <li>
-              <NavLink to='/' className='menu-item'>
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to='/system' className='menu-item'>
-                System
-              </NavLink>
-            </li>
-          </ul>
+      <>
+        <Menu pageWrapId={"page-wrap"} outerContainerId={"app"}>
+          <NavLink to="/" className="menu-item">
+            Strona powitalna
+          </NavLink>
+          <NavLink to="/system" className="menu-item">
+            Informacje o systemie
+          </NavLink>
         </Menu>
-        <main id='page-wrap'>
+
+        <div id="page-wrap">
           <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/system' exact render={() => <p>System</p>} />
+            <Route path="/" exact component={Home} />
+            <Route path="/system" exact render={() => <p>Nie masz systemu biedaku</p>} />
           </Switch>
-        </main>
-      </div>
+        </div>
+      </>
     );
   }
 }
