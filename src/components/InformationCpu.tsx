14f1c2daf2eedb15b25f1ffaa0b32ac223b276e4
Kuba <kuba@email>
2020-04-16 14:29:09 +0200

    Add implementation to InformationCpu component

--- src/components/InformationCpu.tsx
+++ src/components/InformationCpu.tsx
@@ -1,19 +1,97 @@
 import * as React from 'react';
 import { Component } from 'react';
+import { Chart } from "react-google-charts";
+import "../styles/chart.css"
 
-export interface InformationCpuProps {
+const InformationCpu = () => {
 
-}
+    const [CPUusage, setCPUusage] = React.useState("");
+    const [procesorName, setProcesorName] = React.useState("");
+    const [architecture, setArchitecture] = React.useState("");
+    const [numberOfCores, setNumberOfCores] = React.useState("");
 
-export interface InformationCpuState {
 
-}
+    React.useEffect(() => {
+        setInterval(func, 1000);
+    }, [])
 
-class InformationCpu extends React.Component<InformationCpuProps, InformationCpuState> {
-    state = {}
-    render() {
-        return (<div><p>CPU</p></div>);
+    React.useEffect(() => {
+        funcStaticData();
+    }, [])
+
+    const funcStaticData = () => {
+
+        const { exec } = require('child_process');
+        exec('system_monitor_cli.exe --cn', (error: any, stdout: string, stderr: any) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            var value = stdout.toString().split("\n");
+
+            setProcesorName(value[0]);
+            setArchitecture(value[1]);
+            setNumberOfCores(value[2]);
+        });
+
     }
+
+    const func = () => {
+        const { exec } = require('child_process');
+        exec('system_monitor_cli.exe --cpu', (error: any, stdout: string, stderr: any) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            setCPUusage(stdout);
+        });
+    };
+
+    return (
+        <>
+
+            <div className="rowinformation">
+                <div >
+                    <h3>Nazwa procesora:</h3>
+                    <p>{procesorName} </p>
+                </div>
+           
+                <div >
+                    <h3>Architektura procesora:</h3>
+                    <p>{architecture}</p>
+                </div>
+
+                <div >
+                    <h3>Liczba rdzeni:</h3>
+                    <p>{numberOfCores}</p>
+                </div>
+            </div>
+
+
+            <div className="chart-position">
+                <Chart
+                    width={'600px'}
+                    height={'400px'}
+
+                    chartType="PieChart"
+                    loader={<div>Loading Chart</div>}
+                    data={[
+                        ['Task', 'Hours per Day'],
+                        [ 'Wolne CPU',100 - parseInt(CPUusage)],
+                        ['Użycie CPU', parseInt(CPUusage)],
+                    ]}
+                    options={{
+                        title: 'Użycie CPU',
+                        is3D: true,
+                        backgroundColor: 'none',
+                        legendTextStyle: { color: 'grey' },
+                        titleTextStyle: { color: 'grey' },
+                    }}
+                    rootProps={{ 'data-testid': '2' }}
+                />
+            </div>
+        </>
+    )
 }
 
 export default InformationCpu;
