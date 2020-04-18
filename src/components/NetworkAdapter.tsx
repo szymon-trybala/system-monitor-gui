1e11e9bb6d2be28297e25397345330b51cca3c22
Kuba <kuba@email>
2020-04-18 16:15:25 +0200

    Add implementation to NetworkAdapter

--- src/components/NetworkAdapter.tsx
+++ src/components/NetworkAdapter.tsx
@@ -1,18 +1,52 @@
 import * as React from 'react';
+import LoadingIcon from './LoadingIcon'
 
-export interface NetworkAdapterProps {
-    
+
+
+const NetworkAdapter = () => {
+
+    const [maxSpeed, setMaxSpeed] = React.useState("");
+    const [nameOfNetworkAdapter, setNameOfNetworkAdapter] = React.useState("");
+
+
+    React.useEffect(() => {
+
+        setInterval(func, 1000);
+    }, [])
+
+    const func = () => {
+        const { exec } = require('child_process');
+        exec('system_monitor_cli.exe -n', (error: any, stdout: string, stderr: any) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            var value = stdout.toString().split("\n");
+
+            setMaxSpeed(value[0]);
+            setNameOfNetworkAdapter(value[1]);
+        });
+
+
+    };
+
+    return (
+
+        <>
+            <div className="rowinformation">
+                <div className="rowinformation_div">
+                    <h3 className="rowinformation_title">Maksymalna prędkość:</h3>
+                    <div className="rowinformation_title">{maxSpeed === "" ? <LoadingIcon /> : maxSpeed + 'KB/s'}</div>
+
+                </div>
+                <div className="rowinformation_div">
+                    <h3 className="rowinformation_title">Nazwa karty sieciowej:</h3>
+                    <div className="rowinformation_title">{nameOfNetworkAdapter === "" ? <LoadingIcon /> : nameOfNetworkAdapter}</div>
+                </div>
+            </div>
+
+        </>
+    )
 }
- 
-export interface NetworkAdapterState {
-    
-}
- 
-class NetworkAdapter extends React.Component<NetworkAdapterProps, NetworkAdapterState> {
-    state = {   }
-    render() { 
-        return ( <div><p>Adapter sieciowy</p></div> );
-    }
-}
- 
+
 export default NetworkAdapter;
