d2e0b4eaa59a91e30415d021c31a8c6d7fe101a3
Norvos <adrian42603@gmail.com>
2020-04-20 14:32:25 +0200

    Add home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,16 @@
-import * as React from 'react';
-
+import * as React from "react";
+import '../styles/home.css'
 export const Home = () => {
   return (
-    <div>
-      <p> Witam w aplikacji System Monitor :)</p>
-    </div>
-  )
-}
+     <>
+      <div id="home">
+        <p className="size-60 medium title">System Monitor</p>
+        <p className="size-20">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quam adipisci quasi tenetur in amet inventore similique deserunt unde natus!
+        </p>
+      </div>
+
+      <div id="lowerright">Szymon Trybała Jakub Borys Adrian Treffon</div>
+   </>
+  );
+};
